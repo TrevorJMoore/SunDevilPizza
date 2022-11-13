2399e3b0987d109ae7f9f070bc15599c60226abe
TrevorJMoore <moore3112000@hotmail.com>
2022-11-13 12:46:36 -0700

    Preparing for Order Accessing
    Created a class for Orders that hold a shoppingcart.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,8 @@ module com.sundevilpizza.sundevilpizza {
     requires javafx.fxml;
             
         requires org.controlsfx.controls;
-                        
+    requires java.sql;
+
     opens com.sundevilpizza.sundevilpizza to javafx.fxml;
     exports com.sundevilpizza.sundevilpizza;
 }
